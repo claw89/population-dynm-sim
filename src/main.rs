5e8130e24f9dd8b5576d43f4c4f3ffc5c2467593
Christoher Law <christopher.law89@gmail.com>
2024-04-03 21:40:37 +0000

    add timer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use csv;
 use ndarray::{Array, Axis};
 use simulate::*;
+use std::time::Instant;
 
 fn main() {
     let mut rdr = csv::Reader::from_path("data/species_params.csv").unwrap();
@@ -12,9 +13,11 @@ fn main() {
     );
 
     let species_ids: Vec<usize> = vec![0, 1];
-
+    let now = Instant::now();
     let mut population = Population::new(species_array.select(Axis(0), &species_ids).to_vec());
-    population.plot("images/initial.png");
-    population.simulate(31.0);
-    population.plot("images/final.png");
+    // population.plot("images/initial.png");
+    population.simulate(30.0);
+    // population.plot("images/final.png");
+    let elapsed_time = now.elapsed();
+    println!("Completed in {}", elapsed_time.as_secs());
 }
